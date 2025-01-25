ab4de4a660fd5b443a03a4b511894a2ff12504df
Tudor-Matei <mateitudorconstantin@gmail.com>
2025-01-25 21:08:17 +0200

    chore: rewrite app.tsx to use the new functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { ConnectButton, useAccounts, useSignAndExecuteTransaction, useSuiClient } from "@mysten/dapp-kit";
+import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
 import "@mysten/dapp-kit/dist/index.css";
-import { Transaction } from "@mysten/sui/transactions";
-import { burnEthereumIBT } from "./helpers/burn";
+import { convertFromEthereumToSuiTokens } from "./helpers/bridge";
 import useConnectEthereum from "./helpers/connectEthereum";
-import { mintEthereumIBT } from "./helpers/mint";
+import { deploySolidityContract } from "./helpers/deployContract";
+import { mintEthereumIBT, mintSuiIBT } from "./helpers/mint";
 import { sendEth } from "./helpers/sendEth";
 import "./index.css";
 
@@ -16,77 +16,48 @@ declare global {
 const App = () => {
   const { account, ibtBalance, metamaskConnected, error, connect } = useConnectEthereum();
 
-  const suiClient = useSuiClient();
-  const { mutate: signAndExecute } = useSignAndExecuteTransaction();
-  const accounts = useAccounts();
-
-  const mintTokensOnSui = async () => {
-    const tx = new Transaction();
-
-    tx.moveCall({
-      arguments: [
-        tx.pure.address(import.meta.env.VITE_SUI_IBT_TREASURY_CAP),
-        tx.pure.u64(1000),
-        tx.pure.address(accounts[0].address),
-      ],
-      target: `${import.meta.env.VITE_SUI_IBT_CONTRACT}::token::mint`,
+  const handleDeploySolidityContract = () => {
+    deploySolidityContract().then((contractAddress) => {
+      const previousContracts: string | null = localStorage.getItem("ETH_CONTRACTS");
+      localStorage.setItem(
+        "ETH_CONTRACTS",
+        JSON.stringify(!previousContracts ? [contractAddress] : [...JSON.parse(previousContracts), contractAddress])
+      );
     });
-
-    signAndExecute(
-      {
-        transaction: tx,
-      },
-      {
-        onSuccess: async ({ digest }) => {
-          const { effects } = await suiClient.waitForTransaction({
-            digest: digest,
-            options: {
-              showEffects: true,
-            },
-          });
-
-          console.log(effects);
-        },
-      }
-    );
   };
 
-  // const handleDeploySolidityContract = () => {
-  //   deploySolidityContract().then((contractAddress) => {
-  //     const previousContracts: string | null = localStorage.getItem("ETH_CONTRACTS");
-  //     localStorage.setItem(
-  //       "ETH_CONTRACTS",
-  //       JSON.stringify(!previousContracts ? [contractAddress] : [...JSON.parse(previousContracts), contractAddress])
-  //     );
-  //   });
-  // };
-
+  const suiAccount = useCurrentAccount();
   return (
     <div className="App">
-      <h3>Previous solidity contract addresses:</h3>
-      <ul>
-        {localStorage.getItem("ETH_CONTRACTS") &&
-          JSON.parse(localStorage.getItem("ETH_CONTRACTS") as string).map((contract: string, index: number) => (
-            <li key={index}>{contract}</li>
-          ))}
-      </ul>
       <div>
         <button style={{ padding: 10, margin: 10 }} onClick={connect}>
           Connect with MetaMask
         </button>
-        <button onClick={sendEth}>Send ibt</button>
-        {/* <button onClick={handleDeploySolidityContract}>Deploy Solidity contract</button> */}
-        <button onClick={mintEthereumIBT}>Mint IBT tokens on Ethereum</button>
-        <button onClick={burnEthereumIBT}>Burn IBT tokens on Ethereum</button>
+
+        <div>
+          <button disabled onClick={handleDeploySolidityContract}>
+            Deploy Solidity contract
+          </button>
+          <button disabled onClick={mintEthereumIBT}>
+            Mint IBT
+          </button>
+
+          <button disabled onClick={sendEth}>
+            Send ibt
+          </button>
+        </div>
+
+        <button onClick={() => convertFromEthereumToSuiTokens(suiAccount!.address)}>
+          Convert to IBT tokens on SUI
+        </button>
       </div>
 
       <div>
         <ConnectButton connectText="Connect with Sui" />
-        <button onClick={mintTokensOnSui}>Mint IBT tokens on Sui</button>
-        <button onClick={burnEthereumIBT}>Burn IBT tokens on Sui</button>
+        <button onClick={() => mintSuiIBT(suiAccount!.address)}>Mint IBT tokens on Sui</button>
+        <button disabled>Burn IBT tokens on Sui</button>
       </div>
 
-      {/* <button onClick={create}>Call Sui contract</button> */}
       {error && <h3 style={{ color: "red" }}>{error}</h3>}
       {metamaskConnected && (
         <div>
