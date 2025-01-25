863946865fe42d21701320fd6f10425b7b9f0ee9
Tudor-Matei <mateitudorconstantin@gmail.com>
2025-01-25 21:07:44 +0200

    feat: create mint and burn functions for sui

--- src/helpers/burn.ts
+++ src/helpers/burn.ts
@@ -1,21 +1,58 @@
 import { ethers, parseUnits } from "ethers";
-import TokenArtifact from "../../out/Token.s.sol/Token.json"; // Adjust the path to your ABI file
+import TokenArtifact from "../../out/Token.s.sol/Token.json";
 
-export async function burnEthereumIBT() {
-  // Connect to the local Anvil network
+export async function burnEthereumIBT(callback: () => void) {
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
 
-  // Contract address and ABI
   const contractAddress = import.meta.env.VITE_ETH_IBT_CONTRACT;
   const contractABI = TokenArtifact.abi;
 
-  // Create a contract instance
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
   const tx = await contract.burn(parseUnits("1000", 18));
   console.log(tx);
 
   contract.on("BurnEvent", (from, amount) => {
-    alert(`Burned from ${from} amount ${amount}`);
+    callback();
   });
+}
+
+export async function burnSuiIBT(walletAddress: string) {
+  const packageId = import.meta.env.VITE_SUI_IBT_CONTRACT;
+  const treasuryCapId = import.meta.env.VITE_SUI_IBT_TREASURY_CAP;
+
+  try {
+    const response = await fetch("http://localhost:5000/burn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        packageId,
+        treasuryCapId,
+        walletAddress,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    const coinObject = await response.json();
+    if (!coinObject?.coinObjectID) {
+      alert("Error minting tokens. Invalid coin object id.");
+      return null;
+    }
+
+    localStorage.setItem(
+      "coinIDS",
+      JSON.stringify([...JSON.parse(localStorage.getItem("coinIDS") || "[]"), coinObject.coinObjectID])
+    );
+
+    alert("Minted tokens successfully");
+  } catch (error) {
+    console.error("Error minting tokens:", error);
+    alert("Error minting tokens");
+    return null;
+  }
 }
--- src/helpers/mint.ts
+++ src/helpers/mint.ts
@@ -1,21 +1,57 @@
 import { ethers, parseUnits } from "ethers";
-import TokenArtifact from "../../out/Token.s.sol/Token.json"; // Adjust the path to your ABI file
+import TokenArtifact from "../../out/Token.s.sol/Token.json";
 
 export async function mintEthereumIBT() {
-  // Connect to the local Anvil network
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
 
-  // Contract address and ABI
   const contractAddress = import.meta.env.VITE_ETH_IBT_CONTRACT;
   const contractABI = TokenArtifact.abi;
 
-  // Create a contract instance
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
-  const tx = await contract.mint(parseUnits("1000", 18));
-  console.log(tx);
+  await contract.mint(parseUnits("1000", 18));
 
   contract.on("MintEvent", (to, amount) => {
     alert(`Minted to ${to} amount ${amount}`);
   });
+}
+
+export async function mintSuiIBT(walletAddress: string) {
+  const packageId = import.meta.env.VITE_SUI_IBT_CONTRACT;
+  const treasuryCapId = import.meta.env.VITE_SUI_IBT_TREASURY_CAP;
+
+  try {
+    const response = await fetch("http://localhost:5000/mint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        packageId,
+        amount: 1000,
+        treasuryCapId,
+        walletAddress,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    const coinObject = await response.json();
+    if (!coinObject?.coinObjectID) {
+      alert("Error minting tokens. Invalid coin object id.");
+      return null;
+    }
+
+    localStorage.setItem(
+      "coinIDS",
+      JSON.stringify([...JSON.parse(localStorage.getItem("coinIDS") || "[]"), coinObject.coinObjectID])
+    );
+    return true;
+  } catch (error) {
+    console.error("Error minting tokens:", error);
+    alert("Error minting tokens");
+    return null;
+  }
 }
