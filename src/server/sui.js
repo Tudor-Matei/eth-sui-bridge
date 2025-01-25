00f42640e9a278fd3ff2425b6a403b7368718ed3
Tudor-Matei <mateitudorconstantin@gmail.com>
2025-01-25 21:06:07 +0200

    feat: add server for sui minting

---
+++ src/server/sui.js
@@ -0,0 +1,117 @@
+import { spawn } from "child_process";
+import express, { json } from "express";
+
+const app = express();
+const port = 5000;
+
+app.use(json());
+
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.post("/mint", (req, res) => {
+  const { packageId, treasuryCapId, amount, walletAddress } = req.body;
+
+  if (!packageId || !treasuryCapId || !walletAddress) {
+    return res.status(400).send("Missing required parameters");
+  }
+
+  const command = "sui";
+  const args = [
+    "client",
+    "call",
+    "--package",
+    packageId,
+    "--module",
+    "token",
+    "--function",
+    "mint",
+    "--args",
+    treasuryCapId,
+    amount,
+    walletAddress,
+    "--gas-budget",
+    "10000000",
+  ];
+
+  const child = spawn(command, args);
+
+  let output = "";
+  let errorOutput = "";
+
+  child.stdout.on("data", (data) => {
+    output += data.toString();
+  });
+
+  child.stderr.on("data", (data) => {
+    errorOutput += data.toString();
+  });
+
+  child.on("close", (code) => {
+    if (code !== 0) {
+      console.error(`Command failed with code ${code}: ${errorOutput}`);
+      return res.status(500).json({ error: `Command failed with code ${code}: ${errorOutput}` });
+    }
+    console.log(`Command output: ${output}`);
+
+    res.json({
+      coinObjectID: output
+        .split("ObjectID")
+        .filter((t) => !t.includes("Object Changes"))[0]
+        .substring(2, 68),
+    });
+  });
+});
+
+app.post("/burn", (req, res) => {
+  const { packageId, treasuryCapId, coinObjectId } = req.body;
+
+  if (!packageId || !treasuryCapId || !coinObjectId) {
+    return res.status(400).json({ error: "Missing required parameters" });
+  }
+
+  const command = "sui";
+  const args = [
+    "client",
+    "call",
+    "--package",
+    packageId,
+    "--module",
+    "token",
+    "--function",
+    "burn",
+    "--args",
+    treasuryCapId,
+    coinObjectId,
+    "--gas-budget",
+    "10000000",
+  ];
+
+  const child = spawn(command, args);
+
+  let output = "";
+  let errorOutput = "";
+
+  child.stdout.on("data", (data) => {
+    output += data.toString();
+  });
+
+  child.stderr.on("data", (data) => {
+    errorOutput += data.toString();
+  });
+
+  child.on("close", (code) => {
+    if (code !== 0) {
+      return res.status(500).send(`Command failed with code ${code}: ${errorOutput}`);
+    }
+    console.log(`Command output: ${output}`);
+    res.status(200).json({ message: "Burn successful" });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
